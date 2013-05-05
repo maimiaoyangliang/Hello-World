eadb021687c3331db85e8d2381a0a5c8d7676cc1
yangliang <yangliang@maimiaotech.com>
2013-05-05 19:34:57 +0800

    test rm

---
+++ mypython.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: yangliang
+@contact: yangliang@MaimiaoTech.com
+@date: 2013-05-05 19:33
+@version: 0.0.0
+@license: Copyright MaimiaoTech.com
+@copyright: Copyright MaimiaoTech.com
+
+"""
+print "this is just a test"
+
+
